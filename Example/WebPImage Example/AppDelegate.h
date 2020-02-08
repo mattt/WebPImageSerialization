031411f3321c126829a0b2193e5873da6b05c652
Tim Oliver <me@timoliver.com.au>
2020-02-08 17:00:22 +0900

    Updated copyright headers for 2020

--- Example/WebPImage Example/AppDelegate.h
+++ Example/WebPImage Example/AppDelegate.h
@@ -1,6 +1,6 @@
 // AppDelegate.h
 //
-// Copyright (c) 2014 – 2019 Mattt (http://mat.tt/)
+// Copyright (c) 2014 – 2020 Mattt (http://mat.tt/)
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
